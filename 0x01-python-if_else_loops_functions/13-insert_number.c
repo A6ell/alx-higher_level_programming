ff31712c72d1e791214ddda8baed33d1a6bf9616
Ab-Ruh <abell.demere@gmail.com>
2023-06-06 19:38:58 +0300

    13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,49 +3,49 @@
 #include "lists.h"
 
 /**
- * insert_node - Inserts a number into a sorted singly linked list
- * @head: Pointer to the head of the linked list
- * @number: Number to be inserted
- *
- * Return: Address of the new node, or NULL if it failed
- */
+* insert_node - Inserts a number into a sorted singly linked list
+* @head: Pointer to the head of the linked list
+* @number: Number to be inserted
+*
+* Return: Address of the new node, or NULL if it failed
+*/
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current, *prev;
+listint_t *new_node, *current, *prev;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->n = number;
-    new_node->next = NULL;
+new_node->n = number;
+new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
 
-    current = *head;
-    prev = NULL;
+current = *head;
+prev = NULL;
 
-    while (current != NULL && current->n < number)
-    {
-        prev = current;
-        current = current->next;
-    }
+while (current != NULL && current->n < number)
+{
+prev = current;
+current = current->next;
+}
 
-    if (prev == NULL)
-    {
-        new_node->next = *head;
-        *head = new_node;
-    }
-    else
-    {
-        prev->next = new_node;
-        new_node->next = current;
-    }
+if (prev == NULL)
+{
+new_node->next = *head;
+*head = new_node;
+}
+else
+{
+prev->next = new_node;
+new_node->next = current;
+}
 
-    return (new_node);
+return (new_node);
 }
 
