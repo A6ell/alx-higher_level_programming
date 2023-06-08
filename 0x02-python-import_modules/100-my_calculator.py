24affffaebf930a8b3d7dd20fa36eec85e1a6cb4
Ab-Ruh <abell.demere@gmail.com>
2023-06-08 21:18:49 +0300

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-
-import sys
-from calculator_1 import add, sub, mul, div
-
-def calculate(a, operator, b):
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
-    if operator not in operators.keys():
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    return operators[operator](a, b)
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    from ast import operator
+    
+    
+    
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    result = calculate(a, operator, b)
-    print("{} {} {} = {}".format(a, operator, b, result))
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+    print("{} {} {} = {}"
+          .format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
