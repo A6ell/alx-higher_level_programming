c001f040778dcbab7e1043556f186c5bc4166f6e
Ab-Ruh <abell.demere@gmail.com>
2023-06-12 16:24:04 +0300

    12

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,91 @@
 #include "lists.h"
 
 /**
-* is_palindrome - Checks if it is a palindrome
-* @head: Pointer to a pointer
-*
-* Return: 0 if it is not a palindrome
-*/
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks linked list
+ * @head: double pointer
+ *
+ * Return: 1 if it is, 0 if not
+ */
 int is_palindrome(listint_t **head)
 {
-listint_t *current = *head;
-int values[1024];
-int i, j;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-if (*head == NULL || (*head)->next == NULL)
-return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-for (i = 0;  current != NULL;  i++)
-{
-values[i] = current->n;
-current = current->next;
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
 
-for (j = 0;  j < i / 2;  j++)
+/**
+ * reverse_is_palindrome - checks linked list
+ * and reverses the linked list back to its original state
+ * @head: double pointer
+ *
+ * Return: 0 if not
+ */
+int reverse_is_palindrome(listint_t **head)
 {
-if (values[j] != values[i - j - 1])
-return (0);
-}
+	int result;
+	listint_t *original_head = *head;
 
-return (1);
+	result = is_palindrome(head);
+	reverse_listint(head);
+	*head = original_head;
+
+	return (result);
 }
