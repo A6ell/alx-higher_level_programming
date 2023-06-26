cb06e7686ecf3879bf338d57c535dd0575765a5a
Ab-Ruh <abell.demere@gmail.com>
2023-06-26 19:15:27 +0300

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
@@ -9,3 +10,4 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         print()
         return count
+
