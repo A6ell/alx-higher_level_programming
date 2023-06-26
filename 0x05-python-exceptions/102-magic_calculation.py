b82eedd2fe913c57b1ed7e546d278a3f35caba8d
Ab-Ruh <abell.demere@gmail.com>
2023-06-26 19:06:13 +0300

    exce

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+def magic_calculation(a, b):
+    result = 0
+    try:
+        for i in range(1, 4):
+            result += a**b / i
+    except ZeroDivisionError:
+        result = a + b
+    except Exception:
+        result = a * b
+    return result
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = 0
+            if i < len(my_list_1) and i < len(my_list_2):
+                if isinstance(my_list_1[i], (int, float)) and isinstance(
+                    my_list_2[i], (int, float)
+                ):
+                    if my_list_2[i] != 0:
+                        result = my_list_1[i] / my_list_2[i]
+                    else:
+                        print("division by 0")
+                else:
+                    print("wrong type")
+            else:
+                print("out of range")
+            new_list.append(result)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(result)
+        except TypeError:
+            print("wrong type")
+            new_list.append(result)
+        except IndexError:
+            print("out of range")
+            new_list.append(result)
+    return new_list
