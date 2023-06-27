652260549bc44d1528c1ef00b85d864d7b5ff305
Ab-Ruh <abell.demere@gmail.com>
2023-06-27 21:17:21 +0300

    100

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,69 +1,41 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-    """
-    Square class represents a square shape.
-    """
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """
-        Initializes a new instance of the Square class.
+        """Initialize a new square.
 
         Args:
-            size: The size of the square (default is 0).
-            position: The position of the square (default is (0, 0)).
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """
-        Getter method for the size attribute.
-
-        Returns:
-            The size of the square.
-        """
+        """Get/set the current size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Setter method for the size attribute.
-
-        Args:
-            value: The size value to set.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        """
-        Getter method for the position attribute.
-
-        Returns:
-            The position of the square.
-        """
+        """Get/set the current position of the square."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """
-        Setter method for the position attribute.
-
-        Args:
-            value: The position value to set.
-
-        Raises:
-            TypeError: If the value is not a tuple of 2 positive integers.
-        """
         if (
             not isinstance(value, tuple)
             or len(value) != 2
@@ -74,42 +46,28 @@ class Square:
         self.__position = value
 
     def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns:
-            The area of the square.
-        """
-        return self.__size**2
+        """Return the current area of the square."""
+        return self.__size * self.__size
 
     def my_print(self):
-        """
-        Prints a representation of the square using the '#' character.
-
-        If the size is 0, prints an empty line. Otherwise, prints the square
-        with the specified position and size.
-        """
+        """Print the square with the # character."""
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
 
     def __str__(self):
-        """
-        Returns a string representation of the square.
-
-        Returns:
-            A string representation of the square.
-        """
-        square_str = []
-        if self.__size == 0:
-            return ""
-        for _ in range(self.__position[1]):
-            square_str.append("")
-        for _ in range(self.__size):
-            square_str.append(" " * self.__position[0] + "#" * self.__size)
-        return "\n".join(square_str)
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ""
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,61 +1,28 @@
 #!/usr/bin/python3
+
+"""Define a MagicClass matching exactly a bytecode """
+
 import math
 
 
 class MagicClass:
-    """
-    MagicClass represents a class with magic properties and methods.
-    """
+    """Represent a circle."""
 
     def __init__(self, radius=0):
-        """
-        Initializes a new instance of the MagicClass.
+        """Initialize a MagicClass.
 
-        Args:
-            radius: The radius of the magic object (default is 0).
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
-        self.radius = radius
-
-    @property
-    def radius(self):
-        """
-        Getter method for the radius attribute.
-
-        Returns:
-            The radius of the magic object.
-        """
-        return self.__radius
-
-    @radius.setter
-    def radius(self, value):
-        """
-        Setter method for the radius attribute.
-
-        Args:
-            value: The radius value to set.
-
-        Raises:
-            TypeError: If the value is not a number (integer or float).
-        """
-        if type(value) not in [int, float]:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        self.__radius = value
+        self.__radius = radius
 
     def area(self):
-        """
-        Calculates the area of the magic object.
-
-        Returns:
-            The area of the magic object.
-        """
+        """Return the area of the MagicClass."""
         return self.__radius**2 * math.pi
 
     def circumference(self):
-        """
-        Calculates the circumference of the magic object.
-
-        Returns:
-            The circumference of the magic object.
-        """
+        """Return The circumference of the MagicClass."""
         return 2 * math.pi * self.__radius
