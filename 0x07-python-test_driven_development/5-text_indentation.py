e209a64064b9ec0172f6cd60d1190e40cdc4890d
Ab-Ruh <abell.demere@gmail.com>
2023-07-07 19:51:43 +0300

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""
+This is the text_indentation module.
+This module takes in 1 argument, text, and
+prints a text with 2 new lines after each of these characters: ., ? and :
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
+
     """
+    prints a text with 2 lines after each of
+    characters: ., ? and :
+    """
+
     if not isinstance(text, str):
-        raise TypeError("The text must be a string")
-
-    cursor = 0
-
-    while cursor < len(text) and text[cursor] == " ":
-        cursor += 1
-
-    while cursor < len(text):
-        print(text[cursor], end="")
-
-        if text[cursor] == "\n" or text[cursor] in ".?:":
-            if text[cursor] in ".?:":
-                print("\n\n")
-            cursor += 1
-
-            while cursor < len(text) and text[cursor] == " ":
-                cursor += 1
-            continue
-
-        cursor += 1
+        raise TypeError("text must be a string")
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    lines = [line.strip() for line in text.split("\n")]
+    print("\n".join(lines))
