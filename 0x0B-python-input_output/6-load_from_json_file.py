524b32cf5f49db9e762698af6ae26ddd1cd6df59
Ab-Ruh <abell.demere@gmail.com>
2023-07-11 22:15:41 +0300

    fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,19 +3,12 @@
 Module that adds all arguments to a Python list and saves it to a file
 """
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import json
 
-try:
-    # Load existing list from file
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    # Create a new list if the file doesn't exist
-    my_list = []
 
-# Add arguments to the list
-my_list.extend(sys.argv[1:])
-
-# Save the updated list to file
-save_to_json_file(my_list, "add_item.json")
+def load_from_json_file(filename):
+    '''
+    this is the function
+    '''
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return json.load(f)
