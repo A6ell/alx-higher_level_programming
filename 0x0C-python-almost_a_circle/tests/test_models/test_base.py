39318aafec63e99248b236dd0359ffd4c7582422
Ab-Ruh <abell.demere@gmail.com>
2023-07-18 00:43:35 +0300

    ex

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,62 +1,73 @@
 #!/usr/bin/python3
-"""
-This module contains test cases for the class Base. It tests the init,
-to_json, and from_json methods.
-        """
+'''
+this module contains tests for the
+load_from_file method of the
+Base class
+'''
+from re import M, S
 import unittest
+import os
+from models.rectangle import Rectangle
+from models.square import Square
 from models.base import Base
 
 
-class TestBase(unittest.TestCase):
-    """
-    This is the class with all the test case methods.
-    """
+class Test_Load_From_File(unittest.TestCase):
+    '''
+    tests for load_from_file of base.py
+    '''
 
+    def test_load_empty_file(self):
+        '''
+        empty file test
+        '''
+        if (os.path.exists("Rectangle.json") is True):
+            os.remove("Rectangle.json")
+        if (os.path.exists("Square.json") is True):
+            os.remove("Square.json")
+        if (os.path.exists("Base.json") is True):
+            os.remove("Base.json")
+        sq1 = Square.load_from_file()
+        self.assertEqual(sq1, [])
+        os.mknod("Rectangle.json")
+        rect1 = Rectangle.load_from_file()
+        self.assertEqual(rect1, [])
 
-def test_init(self):
-    """
-    This tests the init method.
-    """
-    b1 = Base()
-    self.assertEqual(b1.id, 1)
+    def test_load_rectangle(self):
+        '''
+        Test for rectangles
+        '''
+        rect2 = Rectangle(10, 7, 2, 8)
+        rect3 = Rectangle(2, 4)
+        my_list = [rect2, rect3]
+        Rectangle.save_to_file(my_list)
+        my_list_loaded = Rectangle.load_from_file()
+        self.assertEqual(type(my_list), type(my_list_loaded))
+        self.assertEqual(len(my_list), len(my_list_loaded))
+        for i in range(len(my_list)):
+            self.assertEqual(my_list_loaded[i].to_dictionary(),
+                             my_list[i].to_dictionary())
+        os.remove("Rectangle.json")
 
-    b2 = Base()
-    self.assertEqual(b2.id, 2)
+    def test_load_square(self):
+        '''
+        Test for squares
+        '''
+        sq2 = Square(5)
+        sq3 = Square(7, 9, 1)
+        my_list = [sq2, sq3]
+        Square.save_to_file(my_list)
+        my_list_loaded = Square.load_from_file()
+        self.assertEqual(type(my_list), type(my_list_loaded))
+        self.assertEqual(len(my_list), len(my_list_loaded))
+        for i in range(len(my_list)):
+            self.assertEqual(my_list_loaded[i].to_dictionary(),
+                             my_list[i].to_dictionary())
+        os.remove("Square.json")
 
-    b3 = Base(12)
-    self.assertEqual(b3.id, 12)
-
-    b4 = Base()
-    self.assertEqual(b4.id, 3)
-
-    b5 = Base(-5)
-    self.assertEqual(b5.id, -5)
-
-    b6 = Base(6.3)
-    self.assertEqual(b6.id, 6.3)
-
-    b7 = Base(None)
-    self.assertEqual(b7.id, 4)
-
-
-def test_to_json_string(self):
-    """
-    this tests the to json method
-    """
-    c1 = Base()
-    c_dict = c1.to_dictionary()
-    json_dict = Base.to_json_string([c_dict])
-    json_dict2 = Base.to_json_string(None)
-    self.assertEqual(json_dict, '[{"id": 5}]')
-    self.assertEqual(json_dict2, "[]")
-
-
-def test_from_json_string(self):
-    """
-    this method tests the from json method
-    """
-    json_dict = '[{"id": 1}]'
-    list_dicts = Base.from_json_string(json_dict)
-    list_dicts2 = Base.from_json_string(None)
-    self.assertEqual(list_dicts, [{"id": 1}])
-    self.assertEqual(list_dicts2, [])
+    def test_extra_args(self):
+        '''
+        Test for more arguments than is possible
+        '''
+        with self.assertRaises(TypeError):
+            Base.load_from_file(Square)
