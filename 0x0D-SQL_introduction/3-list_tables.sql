27960221cb02daf1f1e0e0d2b1fa3d5d2e08b0d1
Ab-Ruh <abell.demere@gmail.com>
2023-08-15 22:21:35 +0300

    mysql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,2 @@
 -- Lists all tables in a database passed as arg to mysql cmd
-USE mysql;
-SELECT table_name
-FROM information_schema.tables
-WHERE table_type = 'BASE TABLE'
-  AND table_schema = DATABASE();
+SHOW TABLES;
