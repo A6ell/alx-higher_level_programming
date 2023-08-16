68a34c88963213207f07991e698583765eb075e3
Ab-Ruh <abell.demere@gmail.com>
2023-08-16 22:08:06 +0300

    more

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,8 @@
 -- Lists comedy shows 
-SELECT title 
-FROM tv_shows
-WHERE id IN (
-  SELECT show_id
-  FROM tv_show_genres
-  WHERE genre_id = (
-    SELECT id FROM tv_genres WHERE name = 'Comedy'
-  )
-)
-ORDER BY title ASC;
+SELECT tv_shows.title FROM tv_shows
+       INNER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       	  INNER JOIN tv_genres
+	  ON tv_show_genres.genre_id = tv_genres.id
+	  WHERE tv_genres.name = 'Comedy'
+       ORDER BY tv_shows.title;
