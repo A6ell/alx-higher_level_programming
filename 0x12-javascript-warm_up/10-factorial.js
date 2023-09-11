a9b9cf6aee64933e447829ad534b8b685708d1da
Ab-Ruh <abell.demere@gmail.com>
2023-09-11 11:57:09 +0300

    JS

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const arg = parseInt(process.argv[2]);
+const result = factorial(arg);
+
+console.log(result);
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+const numOccurrences = parseInt(process.argv[2]);
+
+if (isNaN(numOccurrences) || numOccurrences < 1) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log('C is fun');
+  }
+}
