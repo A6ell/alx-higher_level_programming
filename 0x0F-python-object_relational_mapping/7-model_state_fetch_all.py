168140a62976458fd18d506984f6ea97f07fd349
Ab-Ruh <abell.demere@gmail.com>
2023-09-16 21:54:01 +0300

    13

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,32 +3,17 @@
 Script that lists all State objects from the database hbtn_0e_6_usa.
 """
 
-import MySQLdb
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
 
-    state_name = sys.argv[4]
-
-    cur = db.cursor()
-
-    query = '''SELECT cities.name FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC'''
-
-    cur.execute(query, (state_name,))
-
-    rows = cur.fetchall()
-
-    print(", ".join([row[0] for row in rows]))
-
-    cur.close()
-    db.close()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
