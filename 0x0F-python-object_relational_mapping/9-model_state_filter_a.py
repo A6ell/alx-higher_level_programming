df95784605d139035dea4a32c52b65dc09a98377
Ab-Ruh <abell.demere@gmail.com>
2023-09-16 22:07:58 +0300

    13

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" prints the first State object from the database.
 """
-Script that lists all State objects containing letter a from the database.
-"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
