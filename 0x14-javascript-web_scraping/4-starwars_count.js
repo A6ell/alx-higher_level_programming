dcb83328761ee1ef9b331c5885c26c764624bcf5
Ab-Ruh <abell.demere@gmail.com>
2023-10-24 20:12:46 +0300

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
+const url = process.argv[2];
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./4-starwars_count.js <API URL>');
-  process.exit(1);
-}
-
-const apiUrl = process.argv[2];
-const characterId = 18; // Wedge Antilles character ID
-
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  } else if (response.statusCode !== 200) {
-    console.error('Error:', `HTTP Status Code ${response.statusCode}`);
-    process.exit(1);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   } else {
-    const filmData = JSON.parse(body);
-    const filmsWithWedgeAntilles = filmData.results.filter((film) => {
-      return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-    });
-
-    console.log(filmsWithWedgeAntilles.length);
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
